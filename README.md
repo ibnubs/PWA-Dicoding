6c26d7f0150e967af377dfd52919935c4c88910e
ibnu <ibnubs@gmail.com>
2019-10-31 00:39:24 +0700

    add learning modul1 and 2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PWA-Dicoding
 PWA-Dicoding support by IDCAMP 2019
+
+
+I made this repo to save the results of my learning and submission from dicoding for IDCAMP Indosat 2019.
---
+++ learning/first-pwa/service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = "firstpwa-v1";
+
+var urlsToCache = [
+    "/",
+    "/nav.html",
+    "/index.html",
+    "/pages/home.html",
+    "/pages/about.html",
+    "/pages/contact.html",
+    "/css/materialize.min.css",
+    "/js/materialize.min.js",
+    "/js/nav.js"
+];
+
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// fungsi untuk menggunakan aset dari cache agar bisa digunakan offline
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches
+            .match(event.request, { cacheName: CACHE_NAME })
+            .then(function (response) {
+                if (response) {
+                    console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                    return response;
+                }
+
+                console.log(
+                    "ServiceWorker: Memuat aset dari server: ",
+                    event.request.url
+                );
+                return fetch(event.request);
+            })
+    );
+});
+
+// Function digunakan untuk mendelete chace jika dilakukan update
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName != CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
